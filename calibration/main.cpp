e1db12390cdd320aa2125c2fe25a6ad8be7049c1
Junsu Jang <junsuj@mac-56-129-09.mit.edu>
2020-02-04 09:41:29 -0500

    update the calibration code to match our data structure

--- calibration/main.cpp
+++ calibration/main.cpp
@@ -4,8 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 using namespace std;
+
+string curPath;
 
 void help(char *argv[]) {
   cout
@@ -37,20 +40,26 @@ void help(char *argv[]) {
       << "\n" << endl;
 }
 
-static void StereoCalib(const char *imageList, int nx, int ny,
+string concatFileName(int camNo, int curFrame)
+{
+    string fileName = "../good_photos/cam_" + std::to_string(camNo) + "_" + std::to_string(curFrame) + ".jpg";
+    return fileName;
+}
+
+static void StereoCalib(int nImages, int nx, int ny,
                         bool useUncalibrated) {
   bool displayCorners = true;
   bool showUndistorted = true;
   bool isVerticalStereo = false; // horiz or vert cams
   const int maxScale = 1;
   const float squareSize = 1.f;
+  int nCam = 2;
 
   // actual square size
-  FILE *f = fopen(imageList, "rt");
-  int i, j, lr;
+  int i, j, k, lr;
   int N = nx * ny;
   cv::Size board_sz = cv::Size(nx, ny);
-  vector<string> imageNames[2];
+  vector<vector<string> > imageNames[2][nImages];
   vector<cv::Point3f> boardModel;
   vector<vector<cv::Point3f> > objectPoints;
   vector<vector<cv::Point2f> > points[2];
@@ -58,79 +67,55 @@ static void StereoCalib(const char *imageList, int nx, int ny,
   bool found[2] = {false, false};
   cv::Size imageSize;
 
-  // READ IN THE LIST OF CIRCLE GRIDS:
-  //
-  if (!f) {
-    cout << "Cannot open file " << imageList << endl;
-    return;
-  }
   for (i = 0; i < ny; i++)
     for (j = 0; j < nx; j++)
       boardModel.push_back(
           cv::Point3f((float)(i * squareSize), (float)(j * squareSize), 0.f));
-  i = 0;
-  for (;;) {
-    char buf[1024];
-    lr = i % 2;
-    if (lr == 0)
-      found[0] = found[1] = false;
-    if (!fgets(buf, sizeof(buf) - 3, f))
-      break;
-    size_t len = strlen(buf);
-    while (len > 0 && isspace(buf[len - 1]))
-      buf[--len] = '\0';
-    if (buf[0] == '#')
-      continue;
-    cv::Mat img = cv::imread(buf, 0);
-    if (img.empty())
-      break;
-    imageSize = img.size();
-    imageNames[lr].push_back(buf);
-    i++;
 
-    // If we did not find board on the left image,
-    // it does not make sense to find it on the right.
-    //
-    if (lr == 1 && !found[0])
-      continue;
-
-    // Find circle grids and centers therein:
-    for (int s = 1; s <= maxScale; s++) {
-      cv::Mat timg = img;
-      if (s > 1)
-        resize(img, timg, cv::Size(), s, s, cv::INTER_CUBIC);
-      // Just as example, this would be the call if you had circle calibration
-      // boards ...
-      //      found[lr] = cv::findCirclesGrid(timg, cv::Size(nx, ny),
-      //      corners[lr],
-      //                                      cv::CALIB_CB_ASYMMETRIC_GRID |
-      //                                          cv::CALIB_CB_CLUSTERING);
-      //...but we have chessboards in our images
-      found[lr] = cv::findChessboardCorners(timg, board_sz, corners[lr]);
-
-      if (found[lr] || s == maxScale) {
-        cv::Mat mcorners(corners[lr]);
-        mcorners *= (1. / s);
-      }
-      if (found[lr])
+  for (k = 1; k <= nImages; k++) {
+    for (lr = 0; lr < nCam; lr++) {
+      string imgPath = concatFileName(lr+1, k);
+      imageNames[lr][k] = imgPath;
+      cv::Mat img = cv::imread(imgPath, 0);
+      if (img.empty())
         break;
-    }
-    if (displayCorners) {
-      cout << buf << endl;
-      cv::Mat cimg;
-      cv::cvtColor(img, cimg, cv::COLOR_GRAY2BGR);
+      imageSize = img.size();
 
-      // draw chessboard corners works for circle grids too
-      cv::drawChessboardCorners(cimg, cv::Size(nx, ny), corners[lr], found[lr]);
-      cv::imshow("Corners", cimg);
-      if ((cv::waitKey(0) & 255) == 27) // Allow ESC to quit
-        exit(-1);
-    } else
-      cout << '.';
-    if (lr == 1 && found[0] && found[1]) {
-      objectPoints.push_back(boardModel);
-      points[0].push_back(corners[0]);
-      points[1].push_back(corners[1]);
+      // Find circle grids and centers therein:
+      for (int s = 1; s <= maxScale; s++) {
+        cv::Mat timg = img;
+        if (s > 1)
+          resize(img, timg, cv::Size(), s, s, cv::INTER_CUBIC);
+        // Just as example, this would be the call if you had circle calibration
+        // boards ...
+        //      found[lr] = cv::findCirclesGrid(timg, cv::Size(nx, ny),
+        //      corners[lr],
+        //                                      cv::CALIB_CB_ASYMMETRIC_GRID |
+        //                                          cv::CALIB_CB_CLUSTERING);
+        //...but we have chessboards in our images
+        found[lr] = cv::findChessboardCorners(timg, board_sz, corners[lr]);
+
+        if (found[lr] || s == maxScale) {
+          cv::Mat mcorners(corners[lr]);
+          mcorners *= (1. / s);
+        }
+        if (found[lr])
+          break;
+      }
+      if (displayCorners) {
+
+        // draw chessboard corners works for circle grids too
+        cv::drawChessboardCorners(img, cv::Size(nx, ny), corners[lr], found[lr]);
+        cv::imshow("Corners", img);
+        if ((cv::waitKey(0) & 255) == 27) // Allow ESC to quit
+          exit(-1);
+      } else
+        cout << '.';
+      if (lr == 1 && found[0] && found[1]) {
+        objectPoints.push_back(boardModel);
+        points[0].push_back(corners[0]);
+        points[1].push_back(corners[1]);
+      }
     }
   }
   fclose(f);
@@ -224,52 +209,28 @@ static void StereoCalib(const char *imageList, int nx, int ny,
     // RECTIFY THE IMAGES AND FIND DISPARITY MAPS
     //
     cv::Mat pair;
-    if (!isVerticalStereo)
-      pair.create(imageSize.height, imageSize.width * 2, CV_8UC3);
-    else
-      pair.create(imageSize.height * 2, imageSize.width, CV_8UC3);
-
+    pair.create(imageSize.height, imageSize.width * 2, CV_8UC3);
     // Setup for finding stereo corrrespondences
     //
     cv::Ptr<cv::StereoSGBM> stereo = cv::StereoSGBM::create(
         -64, 128, 11, 100, 1000, 32, 0, 15, 1000, 16, cv::StereoSGBM::MODE_HH);
 
     for (i = 0; i < nframes; i++) {
-      cv::Mat img1 = cv::imread(imageNames[0][i].c_str(), 0);
-      cv::Mat img2 = cv::imread(imageNames[1][i].c_str(), 0);
+      cv::Mat img1 = cv::imread(imageNames[0][i], 0);
+      cv::Mat img2 = cv::imread(imageNames[1][i], 0);
       cv::Mat img1r, img2r, disp, vdisp;
       if (img1.empty() || img2.empty())
         continue;
       cv::remap(img1, img1r, map11, map12, cv::INTER_LINEAR);
       cv::remap(img2, img2r, map21, map22, cv::INTER_LINEAR);
-      if (!isVerticalStereo || !useUncalibrated) {
 
-        // When the stereo camera is oriented vertically,
-        // Hartley method does not transpose the
-        // image, so the epipolar lines in the rectified
-        // images are vertical. Stereo correspondence
-        // function does not support such a case.
-        stereo->compute(img1r, img2r, disp);
-        cv::normalize(disp, vdisp, 0, 256, cv::NORM_MINMAX, CV_8U);
-        cv::imshow("disparity", vdisp);
-      }
-      if (!isVerticalStereo) {
-        cv::Mat part = pair.colRange(0, imageSize.width);
-        cvtColor(img1r, part, cv::COLOR_GRAY2BGR);
-        part = pair.colRange(imageSize.width, imageSize.width * 2);
-        cvtColor(img2r, part, cv::COLOR_GRAY2BGR);
-        for (j = 0; j < imageSize.height; j += 16)
-          cv::line(pair, cv::Point(0, j), cv::Point(imageSize.width * 2, j),
-                   cv::Scalar(0, 255, 0));
-      } else {
-        cv::Mat part = pair.rowRange(0, imageSize.height);
-        cv::cvtColor(img1r, part, cv::COLOR_GRAY2BGR);
-        part = pair.rowRange(imageSize.height, imageSize.height * 2);
-        cv::cvtColor(img2r, part, cv::COLOR_GRAY2BGR);
-        for (j = 0; j < imageSize.width; j += 16)
-          line(pair, cv::Point(j, 0), cv::Point(j, imageSize.height * 2),
-               cv::Scalar(0, 255, 0));
-      }
+      cv::Mat part = pair.colRange(0, imageSize.width);
+      cvtColor(img1r, part, cv::COLOR_GRAY2BGR);
+      part = pair.colRange(imageSize.width, imageSize.width * 2);
+      cvtColor(img2r, part, cv::COLOR_GRAY2BGR);
+      for (j = 0; j < imageSize.height; j += 16)
+        cv::line(pair, cv::Point(0, j), cv::Point(imageSize.width * 2, j),
+                  cv::Scalar(0, 255, 0));
       cv::imshow("rectified", pair);
       if ((cv::waitKey() & 255) == 27)
         break;
@@ -291,12 +252,18 @@ static void StereoCalib(const char *imageList, int nx, int ny,
 
 int main(int argc, char **argv) {
   help(argv);
-  int board_w = 9, board_h = 6;
-  const char *board_list = "../stereoData/example_19-03_list.txt";
+  char cCurrentPath[FILENAME_MAX];
+  if (!getcwd(cCurrentPath, sizeof(cCurrentPath)))
+  {
+      return errno;
+  }
+  curPath = string(cCurrentPath);
+  int board_w = 9, board_h = 5;
+  int nImages;
   if (argc == 4) {
-    board_list = argv[1];
+    nImages = atoi(argv[1]);
     board_w = atoi(argv[2]);
     board_h = atoi(argv[3]);
   }
-  StereoCalib(board_list, board_w, board_h, true);
+  StereoCalib(nImages, board_w, board_h, true);
   return 0;
