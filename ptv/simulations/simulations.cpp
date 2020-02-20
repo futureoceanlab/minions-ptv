e604f5263e15d6622e9f56daeade8c77f4fdb938
JunSu Jang <junsuj@mit.edu>
2020-02-19 20:38:50 -0500

    added reading extrinsics and intrinsics high level top down code

--- ptv/simulations/simulations.cpp
+++ ptv/simulations/simulations.cpp
@@ -10,9 +10,30 @@
 using namespace std;
 
 
+// Function declaration
+void readInstrinsics(cv::Mat M, cv::Mat D, string intrinsicsPath);
+void readExtrinsics(cv::Mat R, cv::Mat T, string extrinsicsPath);
+
+
 int main(int argc, char **argv)
 {
-    // intrinsics file(left and right), extrinsics file, output directory
+    // intrinsics file(left and right), 
+    // extrinsics file, output directory
+    if (arc == 5)
+    {
+        string intrinsicsPath1 = argv[1];
+        string intrinsicsPath2 = argv[2];
+        string extrinsicsPath = argv[3];
+        string outputDir = argv[4];
+        createSimulatedParticles(intrinsicsPath1, 
+                                intrinsicsPath2, 
+                                extrinsicsPath, 
+                                outputDir);
+    }
+    else 
+    {
+        cout << "Not enough arguments" << endl;
+    }
     return 0;
 }
 
@@ -45,13 +66,76 @@ int main(int argc, char **argv)
  * 1000um:
  * 
  */
-void createSimulatedParticles() 
+void createSimulatedParticles(string intrinsicsPath1, 
+                              string intrinsicsPath2, 
+                              string extrinsicsPath,
+                              string outputDir) 
 {
     // 1. read the intrinsics and extrinsics files from the arguments
+    // R is rotational matrix and T translational matrix
+    // in reference to the left camera
+    cv::Mat M1, D1, M2, D2, R, T; 
+    readIntrinsics(M1, D1, intrinsicsPath1);
+    readIntrinsics(M2, D2, intrinsicsPath2);
+    readExtrinsics(R, T, extrinsicsPath);
+    
     // 2. Initialize the first frame
-    // 3. based on the velocity with some noise, update the particles position in
-    //    3D position and re-project them onto the imaging plane.
-    // 4. save the particle positions as well as their images onto the output directory
-    saveSimulations(img1List, img2List);
+    vector<vector<cv::Point3f>> particleObjectPoints;
+    vector<vector<cv::Point2f>> particleImagePoints[2];
+
+    initializeParticlePositions(particleObjectPoints);
+    projectObjectsToImages(particleObjectPoints, particleImagePoints, 
+                           M1, D1, M2, D2, R, T);
+
+    // 3. based on the velocity with some noise, update the particles position
+    // in 3D position and re-project them onto the imaging plane.
+    for (int i = 0; i < 3; i++) 
+    {
+        updateParticlePositions(particleObjectPoints, particleImagePoints);
+        projectObjectsToImages(particleObjectPoints, particleImagePoints, 
+                               M1, D1, M2, D2, R, T);    
+    }
+    // 4. plot the particles on the image planes
+    vector<cv::Mat> particleImages[2];
+    drawParticles(particleImagePoints);
+    // 5. save the particle positions as well as their images onto the output directory
+    saveSimulations(particleImages);
     return;
 }
+
+/**
+ * Read intrinsics file
+ */
+void readInstrinsics(cv::Mat M, cv::Mat D, string intrinsicsPath)
+{
+    cv::FileStorage intrinsicsFile(intrinsicsPath, cv::FileStorage::READ);
+    if (intrinsicsFile.isOpned())
+    {
+        intrinsicsFile["M"] >> M;
+        intrinsicsFile["D"] >> D;
+    }
+    else 
+    {
+        cout << "Could not open the intrinsics file " << intrinsicsPath << endl; 
+    }
+    return;
+}
+
+/**
+ * Read extrinsics file
+ */
+void readExtrinsics(cv::Mat R, cv::Mat T, string extrinsicsPath)
+{
+    cv::FileStorage extrinsicsFile(extrinsicsPath, cv::FileStorage::READ);
+    if (extrinsicsFile.isOpned())
+    {
+        extrinsicsFile["R"] >> R;
+        extrinsicsFile["T"] >> T;
+    }
+    else 
+    {
+        cout << "Could not open the extrinsics file " << extrinsicsPath << endl; 
+    }
+    return;
+}
+
