e1bead57f0a4fbe00aa2d67ed2e8ec281bad8aa4
JunSu Jang <junsuj@mit.edu>
2020-03-09 18:30:29 -0400

    working particle simulation first version

--- calibration/simulateParticles.m
+++ calibration/simulateParticles.m
@@ -4,113 +4,148 @@ cameraMatrix1 = cameraMatrix(params.CameraParameters1, eye(3), [0 0 0]);
 cameraMatrix2 = cameraMatrix(params.CameraParameters2, ...
     params.RotationOfCamera2, params.TranslationOfCamera2);
 F = params.FundamentalMatrix;
-
-% Create simulated particles in 3D
-% origin is the center of the camera X-axis points right, Y-axis points bototm 
-nParticles = 100;
-rng(0,'twister');
-xmin = -50;
-xmax = 50;
-ymin = -50;
-ymax = 50;
-zmin = 220;
-zmax = 280;
-xPts = (xmax-xmin).*rand(nParticles,1) + xmin;
-yPts = (ymax-ymin).*rand(nParticles,1) + ymin;
-zPts = (zmax-zmin).*rand(nParticles,1) + zmin;
-centroids = [xPts yPts zPts];
-lPts = projectPoints(centroids, cameraMatrix1');
-rPts = projectPoints(centroids, cameraMatrix2');
-imageSizeY = 1944;
-imageSizeX = 2592;
-lImg = zeros(1944, 2592);
-rImg = zeros(1944, 2592);
-iParticle = 1;
-matchedPoints1 = [];
-matchedPoints2 = [];
-[columnsInImage rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
-radius = 5;
-
-% for i = 1:nParticles
-%   xl = floor(lPts(1, i));
-%   yl = floor(lPts(2, i));
-%   xr = floor(rPts(1, i));
-%   yr = floor(rPts(2, i));
-%   radius = 5;
-%    if isInImage(lPts, i) && isInImage(rPts, i)
-%       iParticle = iParticle + 1; 
-%       if mod(iParticle, 5) == 2
-%           radius = 5 + iParticle;
-%       end
-%       matchedPoints1 = [matchedPoints1; xl yl];
-%       matchedPoints2 = [matchedPoints2; xr yr];
-%    end
-%    
-%    if isInImage(lPts, i)
-%       circlePixels = (rowsInImage - yl).^2 ...
-%     + (columnsInImage - xl).^2 <= radius.^2;
-%       lImg(circlePixels) = 255;
-%    end
-%    if isInImage(rPts, i)
-%       circlePixels = (rowsInImage - yr).^2 ...
-%     + (columnsInImage - xr).^2 <= radius.^2;
-%       rImg(circlePixels) = 255;
-%    end
-%    
-% 
-% end
-% epiLines = epipolarLine(F, matchedPoints1);
-% points = lineToBorderPoints(epiLines,size(rImg));
-
 f1 = params.CameraParameters1.FocalLength;
 p1 = params.CameraParameters1.PrincipalPoint;
 pxPitch = 0.0022;
 intrinsics = [f1(1), 0, p1(1);
                       0, f1(2), p1(2);
                       0, 0, 1]; % mm
-% xl=1086.93177507917; yl = 1244.60319725707; %zl=233.5553;
-% [x, y, z] = sphere(20);
-% spherePts = [x(:), y(:), z(:)]/10;
-% spherePts = bsxfun(@plus,spherePts,centroids(17, :));
-nPoints = 1e4;
-A=rand(2,nPoints);
-R= 0.5;
-H= 1;
-P=(2*pi).*A(1,1:end);
-xc= R.*cos(P);
-yc= R.*sin(P);
-zc= H.*A(2,1:end);
-% random radius 
-r = rand([1, nPoints/2]).* R;
-xc2 = r.*cos(P(1:nPoints/2)); yc2 = r.*sin(P(1:nPoints/2));
-zc2 = zeros(1, nPoints/2);
-zc3 = ones(1, nPoints/2).* H;
-xc = [xc xc2 xc2]';
-yc = [yc yc2 yc2]';
-zc = [zc zc2 zc3]';
-yawD = deg2rad(45); pitchD = deg2rad(30); rollD = deg2rad(0);
-rotX = [cos -sin 0
-% plot3(xc, yc, zc, '.');
-% t = 0:pi/10:2*pi;
-% [xc, yc, zc] = cylinder(2+cos(t), 1e5);
-cylinderPts = [xc yc zc];
-cylinderPts = bsxfun(@plus,cylinderPts,centroids(17, :));
-lPts = projectPoints(cylinderPts, cameraMatrix1');
-rPts = projectPoints(cylinderPts, cameraMatrix2');
-for i=1:size(cylinderPts, 1)
+                  
+rng(1,'twister');
+
+% Create simulated particles in 3D
+% origin is the center of the camera X-axis points right, Y-axis points bototm 
+nParticles = 200;
+centroids = generateCentroids(nParticles, -50, 50, -50, 50, 220, 280);
+% lPts = projectPoints(centroids, cameraMatrix1');
+% rPts = projectPoints(centroids, cameraMatrix2');
+imageSizeY = 1944;
+imageSizeX = 2592;
+lImg = zeros(1944, 2592) + 50;
+rImg = zeros(1944, 2592) + 50;
+iParticle = 1;
+matchedPoints1 = [];
+matchedPoints2 = [];
+[columnsInImage rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
+
+%% Filter out the particles in the imaging frame
+lPts = projectPoints(centroids, cameraMatrix1');
+rPts = projectPoints(centroids, cameraMatrix2');
+newCentroids = [];
+for i = 1:nParticles
     xl = floor(lPts(1, i));
     yl = floor(lPts(2, i));
     xr = floor(rPts(1, i));
     yr = floor(rPts(2, i));
-    lImg(yl, xl) = 255;
-    rImg(yr, xr) = 255;
-%     circlePixels = (rowsInImage - yl).^2 ...
+    if isInImage(lPts, i) || isInImage(rPts, i)
+        newCentroids = [newCentroids; centroids(i, :)];
+    end
+end
+
+%% Generate 3D point clouds given the shape (cylinder + sphere) per centroids
+nParticles = size(newCentroids, 1);
+nCylinders = 10;
+nSpheres = nParticles - nCylinders;
+nCylinderPts = 1e6;
+nSpherePts = 1e3;
+
+% All in mm
+cHeightMin = 0.3; cHeightMax = 4;
+cRadiusMin = 0.1; cRadiusMax = 2.5;
+sDiameterMin = 0.05; sDiameterMax = 1;
+
+cHeights = (cHeightMax - cHeightMin) * rand([nCylinders, 1]) + cHeightMin;
+cRadii = (cRadiusMax - cRadiusMin) * rand([nCylinders, 1]) + cRadiusMin;
+sDiameters = (sDiameterMax - sDiameterMin) * rand([nSpheres, 1]) + sDiameterMin;
+
+% cylinders
+yawD = 0; 
+pitchD = 90*rand([nCylinders, 1]) -45;
+rollD = 90*rand([nCylinders, 1]) - 45;
+cylinders = zeros(nCylinders, nCylinderPts, 3);
+spheres = zeros(nSpheres, (nSpherePts+1)^2, 3);
+
+for i = 1:nParticles
+    if (i <= nCylinders)
+        cylinderPts = generateCylinder(cRadii(i), cHeights(i), nCylinderPts);
+        cylinderPts = rotateCloud(cylinderPts, yawD, pitchD(i), rollD(i));
+        cylinderPts = bsxfun(@plus, cylinderPts, newCentroids(i, :));
+        cylinders(i, :, :) = cylinderPts;
+        lPts = projectPoints(cylinderPts, cameraMatrix1');
+        rPts = projectPoints(cylinderPts, cameraMatrix2');
+        for j = 1:size(lPts, 2)
+            xl = floor(lPts(1, j))+1;
+            yl = floor(lPts(2, j))+1;
+            if isInImage(lPts, j)
+                lImg(yl, xl) = 255;
+            end
+        end
+        for j = 1:size(rPts, 2)
+            xr = floor(rPts(1, j))+1;
+            yr = floor(rPts(2, j))+1;
+            if isInImage(rPts, j)
+                rImg(yr, xr) = 255;
+            end
+        end
+    else
+        k = i - nCylinders;
+        spherePts = generateSphere(nSpherePts, sDiameters(k));
+        spherePts = bsxfun(@plus, spherePts, newCentroids(i, :));
+        spheres(k, :, :) = spherePts;
+        lPts = projectPoints(spherePts, cameraMatrix1');
+        rPts = projectPoints(spherePts, cameraMatrix2');
+        for j = 1:size(lPts, 2)
+            xl = floor(lPts(1, j))+1;
+            yl = floor(lPts(2, j))+1;
+            if isInImage(lPts, j)
+                lImg(yl, xl) = 255;
+            end
+        end
+        for j = 1:size(rPts, 2)
+            xr = floor(rPts(1, j))+1;
+            yr = floor(rPts(2, j))+1;
+            if isInImage(rPts, j)
+                rImg(yr, xr) = 255;
+            end
+        end
+    end
+end
+
+%     if isInImage(lPts, i) && isInImage(rPts, i)
+%       matchedPoints1 = [matchedPoints1; xl yl];
+%       matchedPoints2 = [matchedPoints2; xr yr];
+%     end
+% 
+%     if isInImage(lPts, i)
+%       circlePixels = (rowsInImage - yl).^2 ...
 %     + (columnsInImage - xl).^2 <= radius.^2;
 %       lImg(circlePixels) = 255;
-%             circlePixels = (rowsInImage - yr).^2 ...
+%     end
+%     if isInImage(rPts, i)
+%       circlePixels = (rowsInImage - yr).^2 ...
 %     + (columnsInImage - xr).^2 <= radius.^2;
 %       rImg(circlePixels) = 255;
-end
+%     end
+% epiLines = epipolarLine(F, matchedPoints1);
+% points = lineToBorderPoints(epiLines,size(rImg));
+
+
+% xl=1086.93177507917; yl = 1244.60319725707; %zl=233.5553;
+
+% for i=1:size(cylinderPts, 1)
+%     xl = floor(lPts(1, i));
+%     yl = floor(lPts(2, i));
+%     xr = floor(rPts(1, i));
+%     yr = floor(rPts(2, i));
+%     lImg(yl, xl) = 255;
+%     rImg(yr, xr) = 255;
+% %     circlePixels = (rowsInImage - yl).^2 ...
+% %     + (columnsInImage - xl).^2 <= radius.^2;
+% %       lImg(circlePixels) = 255;
+% %             circlePixels = (rowsInImage - yr).^2 ...
+% %     + (columnsInImage - xr).^2 <= radius.^2;
+% %       rImg(circlePixels) = 255;
+% end
 % pt3d1 = (230.*inv(intrinsics) * [xl; yl; 1])';
 % pt3d2 = (270.*inv(intrinsics) * [xl; yl; 1])';
 % pt3d3 = (z.*inv(intrinsics) * [xl; yl; 1])';
@@ -129,12 +164,12 @@ end
 % rImg(circlePixels) = 255;
 figure; 
 hold on;
-imshow(lImg);
+imshow(lImg, [0 255]);
 hold off;
 
 figure; 
 hold on;
-imshow(rImg);
+imshow(rImg, [0 255]);
 % line(points(:,[1,3])',points(:,[2,4])');
 hold off;
 
